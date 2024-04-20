74f003e410eec06b83a34a656d8f4085d5d967f2
Cristian Nass <cristian.tirdad@gmail.com>
2024-04-20 17:20:16 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.scss";
+import NavigationBar from "./components/NavigationBar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CoursesPage from "./pages/CoursesPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/courses' element={<CoursesPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,25 @@
+import "./style.scss";
+import { Link } from "react-router-dom";
+const NavigationBar = () => {
+  return (
+    <>
+      <div className='navigation-bar-wrapper'>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/courses"}>Courses</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default NavigationBar;
